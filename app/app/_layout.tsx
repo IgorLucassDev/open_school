3941a24a1096ddb3417ca769c21ba8ddb7b7b464
Igor Lucas <igorlucasdev@gmail.com>
2025-02-04 11:14:02 -0300

    Montserrat font support

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,6 +1,24 @@
 import { Stack } from "expo-router";
 import "./global.css";
+import { useFonts } from 'expo-font';
+import { ActivityIndicator } from "react-native";
 
 export default function RootLayout() {
+
+  const [fontsLoaded] = useFonts({
+    Montserrat: require('../assets/fonts/Montserrat-Regular.ttf'),
+    'Montserrat-Bold': require('../assets/fonts/Montserrat-Bold.ttf'),
+    'Montserrat-ExtraBold': require('../assets/fonts/Montserrat-ExtraBold.ttf'),
+    'Montserrat-Light': require('../assets/fonts/Montserrat-Light.ttf'),
+    'Montserrat-Medium': require('../assets/fonts/Montserrat-Medium.ttf'),
+    'Montserrat-SemiBold': require('../assets/fonts/Montserrat-SemiBold.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator />; // Mostra um loading enquanto carrega
+  }
+  
+
+
   return <Stack />;
 }
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat"], // Regular
+        "montserrat-bold": ["Montserrat-Bold"],
+        "montserrat-extrabold": ["Montserrat-ExtraBold"],
+        "montserrat-light": ["Montserrat-Light"],
+        "montserrat-medium": ["Montserrat-Medium"],
+        "montserrat-semibold": ["Montserrat-SemiBold"],
+      },
+    },
   },
   plugins: [],
 }
